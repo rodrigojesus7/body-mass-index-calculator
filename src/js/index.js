baebd028d98e0ebd786b64164a5294d310aba329
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-03-21 11:40:57 -0300

    refactor: refactor html css js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@ let height = document.querySelector('.height')
 let weight = document.querySelector('.weight')
 let bmi = document.querySelector('.resultContainer__bmiNumber')
 let input = document.querySelectorAll('.input')
+let welcomeText = document.querySelector('.resultContainer__welcomeText')
+let welcomeInstructionText = document.querySelector('.resultContainer__welcomeInstructionText')
+let resultText = document.querySelector('.resultContainer__text')
+let resultDetail = document.querySelector('.resultContainer__detailText')
 
 input.forEach(input => {
     input.addEventListener('input', function () {
@@ -16,7 +20,23 @@ input.forEach(input => {
 
             bmi.textContent = fixedResult
 
+            welcomeText.classList.add('hidden')
+            welcomeInstructionText.classList.add('hidden')
+
+            resultText.classList.remove('hidden')
+            resultDetail.classList.remove('hidden')
+            bmi.classList.remove('hidden')
+
+        } else {
+            welcomeText.classList.remove('hidden')
+            welcomeInstructionText.classList.remove('hidden')
+
+            resultText.classList.add('hidden')
+            resultDetail.classList.add('hidden')
+            bmi.classList.add('hidden')
         }
 
     })
 });
+
+
